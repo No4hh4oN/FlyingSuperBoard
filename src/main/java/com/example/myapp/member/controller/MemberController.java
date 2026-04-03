5141a58ab451f2acdb021d22b342671a1ccb97f1
gyujin00 <ckrbwls1214@naver.com>
2026-04-03 16:24:48 +0900

    회원기능/검증 담당

--- src/main/java/com/example/myapp/member/controller/MemberController.java
+++ src/main/java/com/example/myapp/member/controller/MemberController.java
@@ -1,7 +1,6 @@
 package com.example.myapp.member.controller;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpSession;
+import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -15,6 +14,9 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import com.example.myapp.member.model.Member;
 import com.example.myapp.member.service.IMemberService;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpSession;
+
 @Controller
 public class MemberController {
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -23,13 +25,20 @@ public class MemberController {
 	IMemberService memberService;
 
 	@RequestMapping(value="/member/insert", method=RequestMethod.GET)
-	public String insertMember() {
-		logger.info("/member/insert, GET");
+	public String insertMember(HttpSession session) {
+		String csrfToken = UUID.randomUUID().toString();
+		session.setAttribute("csrfToken", csrfToken);
+		logger.info("/member/insert, GET", csrfToken);
 		return "member/form";
 	}
-	
+
 	@RequestMapping(value="/member/insert", method=RequestMethod.POST)
-	public String insertMember(Member member, HttpSession session, Model model) {
+	public String insertMember(Member member, String csrfToken, HttpSession session, Model model) {
+		if(csrfToken==null || "".equals(csrfToken)) {
+			throw new RuntimeException("CSRF 토큰이 없습니다.");
+		}else if(!csrfToken.equals(session.getAttribute("csrfToken"))) {
+			throw new RuntimeException("잘 못된 접근이 감지되었습니다.");
+		}
 		try {
 			if(!member.getPassword().equals(member.getPassword2())) {
 				model.addAttribute("member", member);
@@ -46,40 +55,39 @@ public class MemberController {
 		session.invalidate();
 		return "home";
 	}
-	
+
 	@RequestMapping(value="/member/login", method=RequestMethod.GET)
 	public String login() {
 		return "member/login";
 	}
-	
+
 	@RequestMapping(value="/member/login", method=RequestMethod.POST)
 	public String login(String userid, String password, HttpSession session, Model model) {
 		Member member = memberService.selectMember(userid);
 		if(member != null) {
 			logger.info(member.toString());
 			String dbPassword = member.getPassword();
-			if(dbPassword.equals(password)) { //비밀번호 일치
-				session.setMaxInactiveInterval(600); //10분
+			if(dbPassword.equals(password)) { // 비밀번호 일치
+				session.setMaxInactiveInterval(600); // 세션 타임아웃 10분
 				session.setAttribute("userid", userid);
 				session.setAttribute("name", member.getName());
 				session.setAttribute("email", member.getEmail());
-			}else { //비밀번호가 다름
-				session.invalidate();
+			}else {	// 비밀번호가 다름
 				model.addAttribute("message", "WRONG_PASSWORD");
 			}
-		}else { //아이디가 없음
+		}else { // 아이디가 없음
 			session.invalidate();
 			model.addAttribute("message", "USER_NOT_FOUND");
 		}
 		return "member/login";
 	}
-	
+
 	@RequestMapping(value="/member/logout", method=RequestMethod.GET)
 	public String logout(HttpSession session, HttpServletRequest request) {
-		session.invalidate(); //로그아웃
+		session.invalidate();
 		return "home";
 	}
-	
+
 	@RequestMapping(value="/member/update", method=RequestMethod.GET)
 	public String updateMember(HttpSession session, Model model) {
 		String userid = (String)session.getAttribute("userid");
@@ -89,12 +97,11 @@ public class MemberController {
 			model.addAttribute("message", "UPDATE_USER_INFO");
 			return "member/update";
 		}else {
-			//userid가 세션에 없을 때 (로그인하지 않았을 때)
 			model.addAttribute("message", "NOT_LOGIN_USER");
 			return "member/login";
 		}
 	}
-	
+
 	@RequestMapping(value="/member/update", method=RequestMethod.POST)
 	public String updateMember(Member member, HttpSession session, Model model) {
 		try{
@@ -109,7 +116,7 @@ public class MemberController {
 			return "member/error";
 		}
 	}
-	
+
 	@RequestMapping(value="/member/delete", method=RequestMethod.GET)
 	public String deleteMember(HttpSession session, Model model) {
 		String userid = (String)session.getAttribute("userid");
@@ -119,12 +126,11 @@ public class MemberController {
 			model.addAttribute("message", "MEMBER_PW_RE");
 			return "member/delete";
 		}else {
-			//userid가 세션에 없을 때 (로그인 하지 않았을 때)
 			model.addAttribute("message", "NOT_LOGIN_USER");
 			return "member/login";
 		}
 	}
-	
+
 	@RequestMapping(value="/member/delete", method=RequestMethod.POST)
 	public String deleteMember(String password, HttpSession session, Model model) {
 		try {
@@ -134,7 +140,7 @@ public class MemberController {
 			if(password != null && password.equals(dbpw)) {
 				member.setPassword(password);
 				memberService.deleteMember(member) ;
-				session.invalidate();//삭제되었으면 로그아웃 처리
+				session.invalidate(); // 회원정보가 삭제되면 로그아웃 처리
 				return "member/login";
 			}else {
 				model.addAttribute("message", "WRONG_PASSWORD");
