b1e1aae5df6ba74f094b7aa96202e0c1b8da0185
gyujin00 <ckrbwls1214@naver.com>
2026-04-03 16:46:22 +0900

    16:46 수정

--- src/main/java/com/example/myapp/member/controller/MemberController.java
+++ src/main/java/com/example/myapp/member/controller/MemberController.java
@@ -8,8 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.myapp.member.model.Member;
 import com.example.myapp.member.service.IMemberService;
@@ -24,44 +26,63 @@ public class MemberController {
 	@Autowired
 	IMemberService memberService;
 
-	@RequestMapping(value="/member/insert", method=RequestMethod.GET)
-	public String insertMember(HttpSession session) {
+
+	@GetMapping("/member/insert")
+	public String insertMember(HttpSession session, Model model) {
 		String csrfToken = UUID.randomUUID().toString();
 		session.setAttribute("csrfToken", csrfToken);
 		logger.info("/member/insert, GET", csrfToken);
+		model.addAttribute("member",new Member());
 		return "member/form";
 	}
 
-	@RequestMapping(value="/member/insert", method=RequestMethod.POST)
-	public String insertMember(Member member, String csrfToken, HttpSession session, Model model) {
-		if(csrfToken==null || "".equals(csrfToken)) {
+
+
+
+	@PostMapping(value="/member/insert")
+	public String memberInsert(@Validated Member member, BindingResult result,
+			String csrfToken, HttpSession session, Model model) {
+		if (csrfToken == null || "".equals(csrfToken)) {
 			throw new RuntimeException("CSRF 토큰이 없습니다.");
-		}else if(!csrfToken.equals(session.getAttribute("csrfToken"))) {
+		} else if (!csrfToken.equals(session.getAttribute("csrfToken"))) {
 			throw new RuntimeException("잘 못된 접근이 감지되었습니다.");
 		}
+
+		if (result.hasErrors()) {
+			model.addAttribute("member", member);
+			return "member/form";
+		}
+
 		try {
-			if(!member.getPassword().equals(member.getPassword2())) {
+			if (!member.getPassword().equals(member.getPassword2())) {
 				model.addAttribute("member", member);
 				model.addAttribute("message", "MEMBER_PW_RE");
 				return "member/form";
 			}
+
 			memberService.insertMember(member);
-		}catch(DuplicateKeyException e) {
+
+		} catch (DuplicateKeyException e) {
 			member.setUserid(null);
 			model.addAttribute("member", member);
 			model.addAttribute("message", "ID_ALREADY_EXIST");
 			return "member/form";
 		}
+
 		session.invalidate();
 		return "home";
 	}
 
-	@RequestMapping(value="/member/login", method=RequestMethod.GET)
+
+
+
+
+	@GetMapping("/member/login")
 	public String login() {
 		return "member/login";
 	}
 
-	@RequestMapping(value="/member/login", method=RequestMethod.POST)
+	@PostMapping("/member/login")
 	public String login(String userid, String password, HttpSession session, Model model) {
 		Member member = memberService.selectMember(userid);
 		if(member != null) {
@@ -82,13 +103,15 @@ public class MemberController {
 		return "member/login";
 	}
 
-	@RequestMapping(value="/member/logout", method=RequestMethod.GET)
+	@GetMapping("/member/logout")
 	public String logout(HttpSession session, HttpServletRequest request) {
 		session.invalidate();
 		return "home";
 	}
 
-	@RequestMapping(value="/member/update", method=RequestMethod.GET)
+
+
+	@GetMapping("/member/update")
 	public String updateMember(HttpSession session, Model model) {
 		String userid = (String)session.getAttribute("userid");
 		if(userid != null && !userid.equals("")) {
@@ -102,8 +125,14 @@ public class MemberController {
 		}
 	}
 
-	@RequestMapping(value="/member/update", method=RequestMethod.POST)
-	public String updateMember(Member member, HttpSession session, Model model) {
+	@PostMapping(value="/member/update")
+	public String updateMember(@Validated Member member, BindingResult	result,HttpSession session, Model model) {
+		if(result.hasErrors())	{
+			model.addAttribute("member",member);
+			return	"member/update";
+
+		}
+
 		try{
 			memberService.updateMember(member);
 			model.addAttribute("message", "UPDATED_MEMBER_INFO");
@@ -117,7 +146,7 @@ public class MemberController {
 		}
 	}
 
-	@RequestMapping(value="/member/delete", method=RequestMethod.GET)
+	@GetMapping("/member/delete")
 	public String deleteMember(HttpSession session, Model model) {
 		String userid = (String)session.getAttribute("userid");
 		if(userid != null && !userid.equals("")) {
@@ -131,7 +160,7 @@ public class MemberController {
 		}
 	}
 
-	@RequestMapping(value="/member/delete", method=RequestMethod.POST)
+	@PostMapping("/member/delete")
 	public String deleteMember(String password, HttpSession session, Model model) {
 		try {
 			Member member = new Member();
